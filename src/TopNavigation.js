b2330be4708ae4022c872e0aba18c87b7991e2cf
Maty42 <mathias@poehling.net>
2019-03-22 17:05:27 +0100

    TopNavigator
    Ergänzt um TopNavigation

--- src/TopNavigation.js
+++ src/TopNavigation.js
@@ -6,6 +6,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 
+import './TopNavigation.css';
 import CustomizedSnackbars from './SnackBar.js';
 import EnhancedTable from './EnhancedTable.js';
 import CheckboxList from './CheckboxList.js';
