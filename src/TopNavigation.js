09f6f3d9be85732ebda3422a85cfdebaea9f9c2e
Maty42 <mathias@poehling.net>
2019-03-22 01:19:28 +0100

    Init01
    Initial Project setup

---
+++ src/TopNavigation.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
+import CustomizedSnackbars from './SnackBar.js';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+class ScrollableTabsButtonAuto extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+            <Tab label="Social Media Konten" />
+            <Tab label="Streaming Dienste" />
+            <Tab label="Speicherdienste" />
+            <Tab label="Software Abos" />
+            <Tab label="Fitness-Apps" />
+            <Tab label="Online-Shops" />
+            <Tab label="e-Mail Konten" />
+            <Tab label="Finanz-Apps" />
+            <Tab label="Sonstige Abos" />
+          </Tabs>
+        </AppBar>
+        {value === 0 && <TabContainer><CustomizedSnackbars /></TabContainer>}
+        {value === 1 && <TabContainer>Item Two</TabContainer>}
+        {value === 2 && <TabContainer>Item Three</TabContainer>}
+        {value === 3 && <TabContainer>Item Four</TabContainer>}
+        {value === 4 && <TabContainer>Item Five</TabContainer>}
+        {value === 5 && <TabContainer>Item Six</TabContainer>}
+        {value === 6 && <TabContainer>Item Seven</TabContainer>}
+        {value === 7 && <TabContainer>Item Eight</TabContainer>}
+        {value === 8 && <TabContainer>Item Nine</TabContainer>}
+      </div>
+    );
+  }
+}
+
+ScrollableTabsButtonAuto.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ScrollableTabsButtonAuto);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ScrollableTabsButtonAuto from './TopNavigation.js';
+import CustomizedSnackbars from './SnackBar.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CustomizedSnackbars />, document.querySelector('#root'));
+ReactDOM.render(<ScrollableTabsButtonAuto />, document.querySelector('#root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//ReactDOM.render(<But />, document.querySelector('#app'));
+//ReactDOM.render(<App />, document.getElementById('root'));
